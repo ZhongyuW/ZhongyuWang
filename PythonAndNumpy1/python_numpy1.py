f8b3becd8015c1b8f78d309fa45f20ae308ae364
Zhongyu <wangz9@oregonstate.edu>
2022-04-15 18:51:54 +0000

    explain your changes, such as: lab1 problem2

--- PythonAndNumpy1/python_numpy1.py
+++ PythonAndNumpy1/python_numpy1.py
@@ -1,6 +1,24 @@
 # python_intro.py
 """Python Essentials: Introduction to Python.
-<Name>
-<Class>
-<Date>
+<Name> Zhongyu Wang
+<Class> Mth 420
+<Date> 4/15/2022
 """
+
+#Problem 1
+r = 10
+x , y = 4, 3
+x,y = 4,3
+pi = 3.14159
+z = x / y
+V = z * pi * (r ** 3)
+print(V)
+
+#Problem 2
+print("Hello, world!") 
+
+#Problem 3
+def oops(i):
+   return i + 1
+
+
